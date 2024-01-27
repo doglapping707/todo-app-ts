30a8e511995c1c56c7e2f0ff28dfdf2a07f28ed7
doglapping707 <doglapping707@doglapping707noMacBook-Pro.local>
2024-01-28 02:29:17 +0900

    revert

--- src/router.tsx
+++ src/router.tsx
@@ -4,44 +4,50 @@ import {
     Route,
     Link
 } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Help from "./pages/Help";
 
 function Router() {
     return (
         <BrowserRouter>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                    <li>
-                        <Link to="/help">help</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Switch>
-                <Route path="/login">
-                    <Login />
-                </Route>
-                <Route path="/help">
-                    <Help />
-                </Route>
-                <Route path="/">
-                    <Home />
-                </Route>
-            </Switch>
-            <footer>
-                <div className="author-text">
-                    <p>Made with ❤️ by <a href="https://github.com/doglapping707" target="_blank"><b>doglapping707</b></a></p>
-                </div>
-            </footer>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/users">Users</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/users">
+                        <Users />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
         </BrowserRouter>
     );
+}
+
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Users() {
+    return <h2>Users</h2>;
 }
 
 export default Router
