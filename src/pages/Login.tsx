b6a102c59ceb684705ad39107a11237b62b16419
doglapping707 <doglapping707@doglapping707noMacBook-Pro.local>
2024-01-28 03:22:56 +0900

    install tailwind

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,8 @@
 function Login() {
     return (
-        <h1>Login Page</h1>
+        <h1 className="text-3xl font-bold underline decoration-pink-500">
+            <h1>Login Page</h1>
+        </h1>
     );
 }
 
