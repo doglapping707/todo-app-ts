23df87e61d50e660a6a14fca7ba31a63f9c17367
doglapping707 <doglapping707@doglapping707noMacBook-Pro.local>
2024-01-28 08:23:36 +0900

    install fontawesome

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,13 @@
 function Home() {
     return (
-        <h1 className="text-3xl font-bold underline decoration-sky-500">
-            <h1>Home Page</h1>
-        </h1>
+        <>
+            <button className="btn btn-outline btn-info">
+                <i className="fa-solid fa-right-from-bracket"></i>
+            </button>
+            <h1 className="text-3xl font-bold underline decoration-sky-500">
+                <h1>Home Page</h1>
+            </h1>
+        </>
     );
 }
 
