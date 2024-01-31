1150c15c351fe58e653c6ef18cd72f812d6c70e2
doglapping707 <doglapping707@doglapping707noMacBook-Pro.local>
2024-01-31 09:40:58 +0900

    Add list area style and create footer

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,6 @@
 function Login() {
     return (
         <>
-            <button className="btn btn-outline btn-secondary">
-                LOG OUT
-            </button>
             <h1 className="text-3xl font-bold underline decoration-pink-500">
                 <p>Login Page</p>
             </h1>
