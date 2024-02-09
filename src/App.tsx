81ff71596f8c5848c2f746f94f2a5e8db7031fca
doglapping707 <doglapping707@doglapping707noMacBook-Pro.local>
2024-02-09 21:31:37 +0900

    Updated to retrieve todos using API when displaying the screen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import Router from "./router";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                retry: false
+            },
+            mutations: {
+                retry: false
+            }
+        }
+    });
+
     return (
-        <Router />
+        <QueryClientProvider client={queryClient}>
+            <Router />
+        </QueryClientProvider>
     );
 }
 
