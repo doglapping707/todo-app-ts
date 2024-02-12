d2bffa0dfbaca422b366964c84062e17f533a3cd
doglapping707 <doglapping707@doglapping707noMacBook-Pro.local>
2024-02-12 22:16:21 +0900

    Add edit modal.

--- src/pages/Home/components/List.tsx
+++ src/pages/Home/components/List.tsx
@@ -27,14 +27,41 @@ function List() {
                         <tr key={task.id}>
                             <td>{task.title.length > 14 ? task.title.slice(0, 14) + "..." : task.title}</td>
                             <td>2024-01-30</td>
-                            <td>completed</td>
+                            <td>Completed</td>
                             <td>
-                                <button className="btn btn-warning btn-sm mr-7">
+                                {/* Edit Button */}
+                                <button className="btn btn-warning btn-sm mr-7" onClick={() => (document.getElementById('edit_modal') as HTMLFormElement).showModal()}>
                                     <span className="ico--edit"></span>
                                 </button>
+
+                                {/* Edit Modal */}
+                                <dialog id="edit_modal" className="modal">
+                                    <div className="modal-box">
+                                        <label className="form-control edit_modal--area_input">
+                                            <div className="label">
+                                                <span className="label-text edit_modal--input-label">Task</span>
+                                            </div>
+                                            <input type="text" placeholder="Type here" className="edit_modal--input mb-20" />
+                                            <div className="label">
+                                                <span className="label-text edit_modal--input-label">Due Date</span>
+                                            </div>
+                                            <input type="date" placeholder="Type here" className="edit_modal--input" />
+                                        </label>
+                                        <div className="modal-action">
+                                            <button className="btn btn-secondary">Save</button>
+                                            <form method="dialog">
+                                                <button className="btn btn-neutral">Close</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </dialog>
+
+                                {/* Done Button */}
                                 <button className="btn btn-success btn-sm mr-7">
                                     <span className="ico--check"></span>
                                 </button>
+
+                                {/* Delete Button */}
                                 <button className="btn btn-error btn-sm">
                                     <span className="ico--trash"></span>
                                 </button>
