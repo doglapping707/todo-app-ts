ce603d4267ca45d65b38e08a8fede0841c0b5e03
doglapping707 <doglapping707@doglapping707noMacBook-Pro.local>
2024-08-16 17:26:59 +0900

    更新・削除機能を追加しました。

--- src/pages/Home/components/EditModal.tsx
+++ src/pages/Home/components/EditModal.tsx
@@ -1,14 +1,33 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { useUpdateTask } from "../../../queries/TaskQuery";
 import { Task } from "../../../types/Task";
 import { CSSTransition } from "react-transition-group";
 
 export default function EditModal(task: Task) {
     const [show, setShow] = useState(false);
 
+    const [editTitle, setEditTitle] = useState(task.title)
+    const updateTask = useUpdateTask()
+    function handleUpdate(e: React.MouseEvent<HTMLButtonElement> | React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        const newTask = { ...task }
+        newTask.title = editTitle;
+        updateTask.mutate({
+            id: task.id,
+            task: newTask
+        }, {
+            onSuccess: () => {
+                setShow(false)
+            },
+        })
+    }
+
+    const nodeRef = useRef(null)
     function openModal() {
         setShow(true);
     }
     function closeModal() {
+        setEditTitle(task.title)
         setShow(false);
     }
 
@@ -18,18 +37,19 @@ export default function EditModal(task: Task) {
             <CSSTransition
                 in={show}
                 timeout={200}
+                nodeRef={nodeRef}
                 classNames="transition"
                 unmountOnExit
             >
-                <div className="overlay">
+                <div className="overlay" ref={nodeRef}>
                     <div className="modal">
                         <h1>Update Task</h1>
-                        <form action="">
-                            <input type="text" className="update_input" placeholder="Input a task title" defaultValue={task.title} />
+                        <form onSubmit={handleUpdate}>
+                            <input type="text" className="update_input" defaultValue={editTitle} onChange={(e) => setEditTitle(e.target.value)} placeholder="Input a task title" />
                         </form>
                         <div className="button_area">
                             <button className="cancel_btn" onClick={closeModal}>Cancelar</button>
-                            <button className="update_btn">Salvar</button>
+                            <button className="update_btn" onClick={handleUpdate}>Salvar</button>
                         </div>
                     </div>
                 </div>
